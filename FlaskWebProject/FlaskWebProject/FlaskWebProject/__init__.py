5667be4ec37dd5aa6c8f2c29ff36a297c3db4fd0
Kasin <kasinsparks@gmail.com>
2019-03-12 03:52:40 -0500

    Added a sqlite data base. Need to create the tables needed now and create the code in python to perform the sql commands

--- FlaskWebProject/FlaskWebProject/FlaskWebProject/__init__.py
+++ FlaskWebProject/FlaskWebProject/FlaskWebProject/__init__.py
@@ -2,8 +2,16 @@
 The flask application package.
 """
 
+import os
 from flask import Flask
+
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'FlaskWebProject', 'Images')
+app.config['DATABASE']  = os.path.join(os.getcwd(), 'FlaskWebProject', 'database', 'db.sqlite')
+
+import FlaskWebProject.db
+db.init_app(app)
 
 import FlaskWebProject.views
 import FlaskWebProject.dhsViews
--- FlaskWebProject/FlaskWebProject/FlaskWebProject/dhsViews.py
+++ FlaskWebProject/FlaskWebProject/FlaskWebProject/dhsViews.py
@@ -7,7 +7,9 @@ import base64
 from datetime import datetime
 from flask import Flask, render_template, request, url_for, send_from_directory, redirect
 from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash, generate_password_hash
 from FlaskWebProject import app
+from FlaskWebProject.db import getDB
 
 #test = Enviroment(app)
 
@@ -15,8 +17,8 @@ from FlaskWebProject import app
 # ignore this for now
 #UPLOAD_FOLDER = 'static/img'
 
-UPLOAD_FOLDER = os.getcwd() + '\\Images'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#UPLOAD_FOLDER = os.getcwd() + '\\FlaskWebProject\\Images'
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def imgGet(image):
 	# print(static/img/NoImageIcon.png)
@@ -24,13 +26,14 @@ def imgGet(image):
 	# return 'static/img/backgound.jpg'
 	# return render_template('imgTest.html', testImg='static/img/backgound.jpg')
 	# return open('static/img/'+image, 'r+b').read()
-	folder = UPLOAD_FOLDER + '\\'
+	## folder = UPLOAD_FOLDER + '\\'
+
 	# with open(folder + image, 'rb') as image_file:
 
 	##print("currDir: " + os.curdir)
 	##print("osPath: " + os.path.join("test", "test2"))
 
-	with open(folder + image, 'rb') as image_file:
+	with open(os.path.join(app.config['UPLOAD_FOLDER'], image), 'rb') as image_file:
 		tempImg = base64.b64encode(image_file.read())
 		tempImg = tempImg.decode('utf-8') # if you start geting no image check here
 										# I had to change this when i converted project to windows
@@ -50,5 +53,11 @@ def result():
 	print(img)
 	filename = secure_filename(img.filename)
 	##savePath = os.getcwd() + app.config['UPLOAD_FOLDER'] + '\\'
-	img.save(os.path.join(UPLOAD_FOLDER, filename))
+	img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+	# test
+	db = getDB()
+	db.execute( 'INSERT INTO Clients (username, password) VALUES (?, ?)', ("JohnDoe", generate_password_hash("password")))
+	db.commit()
+
 	return render_template('imgTest.html', pageName="Image received", img=imgGet(filename))
